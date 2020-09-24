93f9272059152f2eeb95fa0e01cecab467bba6af
thesebonesalone <duikhead2@gmail.com>
2020-09-23 19:26:23 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
   # write code here
+  attr_accessor :sides, :kind
+  def initialize(a,b,c)
+    @sides = [a,b,c]
+    if   @sides.any?{|side| side <= 0 || side >= (@sides.sum - side)}
+      raise TriangleError
+    else
+      
+    end
+    @kind = [nil, :equilateral, :isosceles,:scalene][@sides.uniq.length]
+  end
+    
+
+
+
+
+
+
+  class TriangleError < StandardError
+    def message
+      "Triangle is invalid."
+    end
+  end
 end
